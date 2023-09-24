d0e8c67d5d9474817dfee6cfee5dc1b14c70333a
Stavros Barousis <stavrosbarousis@gmail.com>
2023-09-24 16:14:58 +0300

    Added privacy policy link in info section

--- app/src/main/java/com/junkiedan/junkietuner/core/fragments/InfoFragment.java
+++ app/src/main/java/com/junkiedan/junkietuner/core/fragments/InfoFragment.java
@@ -1,11 +1,19 @@
 package com.junkiedan.junkietuner.core.fragments;
 
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.util.Xml;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.textview.MaterialTextView;
 import com.junkiedan.junkietuner.R;
 
 /**
@@ -44,5 +52,13 @@ public class InfoFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_info, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //Enable links in string resource.
+        MaterialTextView infoTextView = view.findViewById(R.id.infoMaterialTextViewId);
+        infoTextView.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
