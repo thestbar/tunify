0b7425851a9bde115552765df9f21509bda9e979
thestbar <stavrosbarousis@gmail.com>
2026-05-30 13:21:42 +0300

    chore: migrate settings.gradle to Kotlin DSL, add JitPack repo

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,7 +10,8 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jitpack.io") }
     }
 }
 rootProject.name = "JunkieTuner"
-include ':app'
+include(":app")
