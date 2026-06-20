836876a1b30581ad99495bfde74b4060df06f17d
thestbar <stavrosbarousis@gmail.com>
2026-06-20 03:14:54 +0300

    chore: bump version to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,8 +22,8 @@ android {
         applicationId = "dev.thestbar.tunify"
         minSdk = 24
         targetSdk = 36
-        versionCode = 3
-        versionName = "2.0"
+        versionCode = 4
+        versionName = "2.1"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
